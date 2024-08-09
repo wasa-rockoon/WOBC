6dca7c361c50a72c18b42a8e1c06ad5749e14aae
fujiy <mail@fujiy.dev>
2024-08-10 01:06:33 +0900

    ESP32, RP2040サンプル

--- src/modules/RP2040CoreTest/main.cpp
+++ src/modules/RP2040CoreTest/main.cpp
@@ -1,32 +1,33 @@
+// #define NDEBUG
+
 #include <Arduino.h>
 #include <library/wobc.h>
-
 
 class Main: public process::Component {
 public:
   Main(): process::Component("main", 0) {}
 
   void setup() override {
-    Serial.print("START");
+    // printf("Start");
   }
   void loop() override {
-    delay(1000);
+    delay(500);
 
     wcpp::Packet p = newPacket(200);
 
     static unsigned i = 0;
-    switch (i % 4) {
+    switch (i % 1) {
     case 0:
-      p.command('A', 0x11);
+      p.command('A', 0x12);
       break;
     case 1:
-      p.command('B', 0x11, 0x22, 0x33, 12345);
+      p.command('B', 0x12, 0x22, 0x33, 12345);
       break;
     case 2:
-      p.telemetry('C', 0x11);
+      p.telemetry('C', 0x12);
       break;
     case 3:
-      p.telemetry('D', 0x11, 0x22, 0x33, 12345);
+      p.telemetry('D', 0x12, 0x22, 0x33, 12345);
       break;
     }
     p.append("Nu").setNull();
@@ -43,22 +44,40 @@ public:
   }
 };
 
+core::CANBus can_bus(23, 22);
 core::SerialBus serial_bus(Serial);
+core::WatchIndicator<unsigned> status_indicator(25, kernel::packetCount());
+// core::WatchIndicator<unsigned> error_indicator(24, kernel::anomalyCount());
 Main main_;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  serial_bus.begin();
+  delay(500);
+  // enableCore1WDT();
+
+  can_bus.begin();
+  // serial_bus.begin();
+  status_indicator.begin();
+  // error_indicator.begin();
+
+  delay(1000);
+
   main_.begin();
+
+  pinMode(24, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  digitalWrite(24, LOW);
   vTaskDelay(1000);
-
+  digitalWrite(24, HIGH);
+  vTaskDelay(1000);
+  // log_d("[%d %d]", serial_bus.getMaximumStackUsage(), status_indicator.getMaximumStackUsage());
+  // main_.LOG("stack: %d %d %d", F_CPU, serial_bus.getMaximumStackUsage(), can_bus.getMaximumStackUsage());
 }
 
 
