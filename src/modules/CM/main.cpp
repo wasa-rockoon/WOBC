06dd88b1f6881fce08624e657637abe75ff9de40
nassan <sand.ss376@gmail.com>
2025-03-01 19:09:37 +0900

    各種[env]を追加

---
+++ src/modules/CM/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // Initialize the serial port
+  Serial.begin(115200);
+  Serial.println("CM module started");
+  // Initialize the CM module
+}
+void loop() {
+
+}
