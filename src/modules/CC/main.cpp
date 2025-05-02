ac434df1192bb97ebdb599dac4dfe1f158c3cf5a
nassan <sand.ss376@gmail.com>
2025-05-02 18:03:41 +0900

    Reapply "各種[env]を追加"
    This reverts commit ccc804d16412df13b768156cc56868a79d96f36c.

---
+++ src/modules/CC/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // Initialize the serial port
+  Serial.begin(115200);
+  Serial.println("CC module started");
+  // Initialize the CC module
+}
+void loop() {
+
+}
---
+++ src/modules/CD/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+
+void setup() {
+  // Initialize the serial port
+  Serial.begin(115200);
+  Serial.println("CD module started");
+  // Initialize the CD module
+}
+void loop() {
+
+}
