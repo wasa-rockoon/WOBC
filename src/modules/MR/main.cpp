01600cc7581d78b8c45702dc5eda9bf920403750
nassan <sand.ss376@gmail.com>
2025-08-03 19:59:08 +0900

    env作ってbuildするところまで

---
+++ src/modules/MR/main.cpp
@@ -0,0 +1,17 @@
+// #define NDEBUG
+
+#include <library/wobc.h>
+#include <components/LiPoPower/lipo_power.h>
+#include <components/LoRa/lora.h>
+#include <components/Pressure/pressure.h>
+#include <components/GPS/gps.h>
+#include <components/Logger/logger.h>
+#include <SPI.h>
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
