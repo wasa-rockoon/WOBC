7cb987a4f21247ba03ecca8092f4676fc030a915
NARROW88\sands <sand.ss376@gmail.com>
2025-09-08 23:52:35 +0900

    ピン番号設定

--- src/modules/MissionBus/main.cpp
+++ src/modules/MissionBus/main.cpp
@@ -8,10 +8,10 @@
 #include <components/Logger/logger.h>
 #include <SPI.h>
 
-#define SPI0_SCK_PIN 1
-#define SPI0_MOSI_PIN 4
-#define SPI0_MISO_PIN 3
-#define SPI0_CS_PIN 2
+#define SPI0_SCK_PIN 5
+#define SPI0_MOSI_PIN 1
+#define SPI0_MISO_PIN 2
+#define SPI0_CS_PIN 4
 
 #define SD_INSERTED_PIN 5
 #define SDCARD_MOSI_PIN SPI0_MOSI_PIN
@@ -19,20 +19,23 @@
 #define SDCARD_SS_PIN SPI0_CS_PIN
 #define SDCARD_SCK_PIN SPI0_SCK_PIN
 
-#define ST 6
-#define PG 10
-#define STAT1 43
-#define STAT2 44
-#define HEAT 9
-#define CHARGELED 8
-#define TEMP 7
+#define ST 8
+#define PG 11
+#define STAT1 10
+//#define STAT2 
+#define HEAT 48
+//#define CHARGELED 
+#define TEMP 9
 
-#define LORA_CHANNEL 3
-#define LORA_TX_PIN 38
-#define LORA_RX_PIN 39
-#define LORA_AUX_PIN 40
-#define LORA_M0_PIN 12
-#define LORA_M1_PIN 11
+#define LORA_CHANNEL 11
+#define LORA_TX_PIN 13
+#define LORA_RX_PIN 12
+#define LORA_AUX_PIN 21
+#define LORA_M0_PIN 14
+#define LORA_M1_PIN 18
+
+constexpr uint8_t module_id = 0x54;
+constexpr uint8_t unit_id = 0x61;
 
 void setup() {
     // Initialize the MissionBus
