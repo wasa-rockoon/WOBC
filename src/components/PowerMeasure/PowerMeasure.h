358438cfb895bfb6c1fedb67db394ffd4f3b5c8e
m.numata <nasakinumata@gmail.com>
2026-08-30 16:21:22 +0900

    PowerMeasureのコードを途中まで書いた

---
+++ src/components/PowerMeasure/PowerMeasure.h
@@ -0,0 +1,36 @@
+#include <library/wobc.h>
+#include <Wire.h>
+#include "INA226.h"
+
+namespace component {
+
+class PowerMeasure : public process::Component {
+public:
+    static const uint8_t component_id = 0x49;
+    static const uint8_t telemetry_id = "P";
+
+    PowerMeasure(TwoWire& wire, uint8_t addr, unsigned sample_freq_hz);
+
+protected:
+    TwoWire& wire_;
+    INA226 ina_power;
+
+    uint8_t unit_id_;
+
+void setup() override;
+
+class SampleTimer : public process::Timer {
+public:
+    SampleTimer(PowerMeasure& power_measure_ref, INA226& ina_power_ref, uint8_t unit_id_ref, unsigned interval_ms);
+
+protected:
+    void callback() override;
+
+private:
+    INA226& ina_power_;
+    PowerMeasure& power_measure_;
+    uint8_t unit_id_;
+} sample_timer_;
+};
+
+} // namespace component
