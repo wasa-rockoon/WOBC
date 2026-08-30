61d9798964b511c162c536a82796e00cec0f56e3
m.numata <nasakinumata@gmail.com>
2026-08-30 11:51:42 +0900

    パケットのエントリ名を変更

--- src/components/Heater/Heater.cpp
+++ src/components/Heater/Heater.cpp
@@ -77,10 +77,10 @@ namespace component {
         }
         wcpp::Packet packet = newPacket(32);
         packet.telemetry(telemetry_id, component_id(), unit_id_, 0xFF, 1234);
-        packet.append("C1").setFloat32(CalculatedTemperature[0]);
-        packet.append("C2").setFloat32(CalculatedTemperature[1]);
-        packet.append("C3").setFloat32(CalculatedTemperature[2]);
-        packet.append("C4").setFloat32(CalculatedTemperature[3]);
+        packet.append("Ca").setFloat32(CalculatedTemperature[0]);
+        packet.append("Cb").setFloat32(CalculatedTemperature[1]);
+        packet.append("Cc").setFloat32(CalculatedTemperature[2]);
+        packet.append("Bv").setFloat32(CalculatedTemperature[3]);
         packet.append("Ts").setInt((int)millis());
         sendPacket(packet);
     }
